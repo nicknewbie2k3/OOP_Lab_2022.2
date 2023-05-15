b1c390663f1d4ccd1d022a35e606947c6ea4b40e
nicknewbie2k3 <116050319+nicknewbie2k3@users.noreply.github.com>
2023-05-15 21:57:16 +0700

    Add files via upload

---
+++ Store.java
@@ -0,0 +1,20 @@
+package mypackage;
+
+public class Store {
+	DigitalVideoDisc itemsInStore[];
+	int count = 0;
+	public void addDVD(DigitalVideoDisc object) {
+		itemsInStore[count] = object;
+		count++;
+		System.out.println("Added DVD to store successfully!");
+	}
+	public void removeDVD(int i) {
+		int deleted = 0;
+		while (i < count) {
+			itemsInStore[i+1] = itemsInStore[i];
+		}
+		if (deleted == 0) {
+			System.out.println("The requested ID is not found.");
+		} else count--;
+	}
+}
