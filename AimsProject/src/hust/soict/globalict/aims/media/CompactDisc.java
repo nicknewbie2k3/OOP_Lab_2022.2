9017d0cf60e4c6a138ae9634a692821608b489da
nicknewbie2k3 <116050319+nicknewbie2k3@users.noreply.github.com>
2023-06-11 22:32:22 +0700

    Add existing project files to Git

---
+++ AimsProject/src/hust/soict/globalict/aims/media/CompactDisc.java
@@ -0,0 +1,42 @@
+package hust.soict.globalict.aims.media;
+
+import java.util.ArrayList;
+
+public class CompactDisc extends Disc {
+    private String artist;
+    private ArrayList<Track> tracks;
+
+    public CompactDisc(int id, String title, String category, float cost, int length, String director, String artist) {
+        super(id, title, category, cost, length, director);
+        this.artist = artist;
+        this.tracks = new ArrayList<Track>();
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void addTrack(Track track) {
+        if (!tracks.contains(track)) {
+            tracks.add(track);
+        } else {
+            System.out.println("Track is already in the list.");
+        }
+    }
+
+    public void removeTrack(Track track) {
+        if (tracks.contains(track)) {
+            tracks.remove(track);
+        } else {
+            System.out.println("Track is not found in the list.");
+        }
+    }
+
+    public int getLength() {
+        int totalLength = 0;
+        for (Track track : tracks) {
+            totalLength += track.getLength();
+        }
+        return totalLength;
+    }
+}
